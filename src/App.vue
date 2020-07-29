a17ee7575440199f6cb66acea09789cd0cd90ab4
Candra <candradwikusuma07@gmail.com>
2020-07-29 10:41:50 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="NewQuote"></app-new-quote>
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted="quoteDeleted"
+    ></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center mt-3">
+        <div class="alert alert-info">info: Click on a Quote to Delete it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NewQuote from "./component/NewQuote.vue";
+import QuoteGrid from "./component/QuoteGrid.vue";
+import Header from "./component/Header.vue";
+export default {
+  data() {
+    return {
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    NewQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("please delete the quotes first");
+      }
+      this.quotes.push(quote);
+    },
+    quoteDeleted(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  }
+};
+</script>
+
+<style></style>
---
+++ src/component/Quote.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3 mt-3">
+    <div class="card quote">
+      <div class="card-body">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.card-body {
+  font-family: "Arizonia", cursive;
+  font-size: 2em;
+  color: #6e6e6e;
+}
+.quote {
+  cursor: pointer;
+}
+.quote:hover {
+  background-color: #ffe2e2;
+}
+</style>
